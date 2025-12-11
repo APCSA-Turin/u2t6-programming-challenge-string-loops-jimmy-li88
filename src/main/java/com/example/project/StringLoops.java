9152fd61a56f3a06d09f74b2c9b607e3269dd563
Jimmy Li <jli8239@bths.edu>
2025-12-11 01:11:06 +0000

    save

--- src/main/java/com/example/project/StringLoops.java
+++ src/main/java/com/example/project/StringLoops.java
@@ -7,11 +7,11 @@ public class StringLoops {
    //WARM UP 
     // using a for loop remove all 'a' characters from input string
    public String removeA(String str) {
-       for (int i = 0; i < str.length(); i++) {
-           if (str.charAt(i) == 'a' || str.charAt(i) == 'A') {
-               str = str.replace("a", "").replace("A", "");
-           }
-       }
+        for (int i = 0; i < str.length(); i++){
+            if (str.substring(i,i+1).equalsIgnoreCase("a")){
+                str = str.replace("a","");
+            }
+        }
        return str;
    }
 
@@ -19,118 +19,124 @@ public class StringLoops {
    // WARM UP
    // using a while loop remove all 'a' characters from input string
    public String removeA2(String str) {
-    int i = 0;
-        while (str.contains("a")){
-        if (str.charAt(i) == 'a' || str.charAt(i) == 'A') {
-               str = str.replace("a", "").replace("A", "");
-           }
-        }
-        return str;
+       while (str.contains("a") || str.contains("A")) {
+           str = str.replace("a", "").replace("A", "");
+       }
+       return str;
    }
+   
 
 
    //WARM UP 
    //Reverse a string by counting down from the end to the start
    public String reverseString(String str) {
-        String reversed = "";
-        for (int i = str.length() - 1; i >= 0; i--) {
-            reversed += str.charAt(i);
-        }
-       return reversed;
+         String reversed = "";
+         for (int i = str.length() - 1; i >= 0; i--) {
+              reversed += str.charAt(i);
+         }
+         return reversed;
    }
 
 
    //WARM UP
    //Reverse a string by counting up from the start to the end
    public String reverseString2(String str) {
-        String reversed = "";
-        int index = str.length() - 1;
-        while (index >= 0) {
-            reversed += str.charAt(index);
-            index--;
-        }
-        return reversed;
+       
+       String reversed = "";
+         int index = str.length() - 1;
+         while (index >= 0) {
+             reversed += str.charAt(index);
+             index--;
+         }
+        
+         return reversed;
    }
 
 
    // using a for loop
    public String replaceCharacterV1(String searchChar, String origStr, String replaceChar){
-        for (int i = 0; i < origStr.length(); i++)
-            if (origStr.substring(i, i + 1).equals(searchChar)) {
-                origStr = origStr.replace(searchChar, replaceChar);
+        for (int i = 0; i < origStr.length(); i++){
+            if (origStr.substring(i,i+1).equals(searchChar)){
+                origStr =  origStr.replace(searchChar,replaceChar);
             }
+        }
         return origStr;
+        
    }
 
 
    // using a while loop
    public String replaceCharacterV2(String searchChar, String origStr, String replaceChar) {
-    int i = 0; 
-        while (i < origStr.length()){
-            if (origStr.substring(i, i + 1).equals(searchChar)) {
-                origStr = origStr.replace(searchChar, replaceChar);
-            }
-        }
-       return origStr;
+       
+       while (origStr.contains(searchChar)){
+              origStr = origStr.replace(searchChar, replaceChar);
+
+       }
+         return origStr;
    }
 
    public int countString(String searchString, String origString) {
-        int counter = 0;
-        for (int i = 0; i < origString.length() - searchString.length(); i++){
-            if (origString.substring(i, i + searchString.length()).equals(searchString));
-            counter++; 
+    int count = 0;
+        for (int i =0; i < origString.length() - searchString.length() + 1; i++) {
+            if (origString.substring(i, i + searchString.length()).equals(searchString)) {
+                count ++;
+            }
         }
-       return 0;
+
+       return count;
    }
 
    public String removeString(String searchString, String origString) {
-        while (origString.contains(searchString)); {
-            origString = origString.replace(searchString, "");
-        }
+         while (origString.contains(searchString)){
+              origString = origString.replace(searchString, "");
+         }
        return origString;
    }
 
 
    public void commaSeparated(int fromNum, int toNum) { //the tests will compare your printed output (not a returned string)
-    StringBuilder output = new StringBuilder(); 
-    int i = 0;
-        if (fromNum <= toNum) {
-            for (i = fromNum; i <= toNum; i++);
-                output.append(i);
-                if (i < toNum){
-                    output.append(", ");
-                }
-            
-        }
-        else {
-            for (i = fromNum; i >= toNum; i--){
-                output.append(i);
-                if (i > toNum){ 
-                    output.append(", ");
-                }
+    StringBuilder output = new StringBuilder();
+    if (fromNum <= toNum) {
+        for (int i = fromNum; i <= toNum; i++) {
+            output.append(i);
+            if (i < toNum) {
+                output.append(", ");
             }
         }
-       System.out.println();
+    } else {
+        for (int i = fromNum; i >= toNum; i--) {
+            output.append(i);
+            if (i > toNum) {
+                output.append(", ");
+            }
+            
+        }
+       
+   }
+    System.out.println(output.toString());
    }
 
 
-
    public boolean isPalindrome(String myString) {
-        String reversed = "";
-        for (int i = myString.length() - 1; i >= 0; i--) {
-            reversed += myString.charAt(i);
-        }
-        String noSpaces = myString.replace(" ", ""); 
-        String noSpaces2 = reversed.replace(" ", "");
-        if (noSpaces.equals(noSpaces2));
-            return true; 
+       myString = myString.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+       String reversed = "";
+       for (int i = myString.length() - 1; i >= 0; i--) {
+           reversed += myString.charAt(i);
+       }
+       return reversed.equals(myString);
    }
 
 
    public void multiPrint(String toPrint, int num) { //the tests will compare your printed output (not a returned string)
-        for (int i = 0; i <= num; i++){
-            System.out.println(toPrint);
-        }
-       
+       StringBuilder output = new StringBuilder();
+       output.append("[");
+       for (int i = 0; i < num; i++) {
+           output.append(toPrint);
+           if (i < num - 1) {
+               output.append(" ");
+           }
+       }
+       output.append("]");
+       System.out.println(output.toString());
    }
 }
